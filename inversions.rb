2721a3dfd9c3215c85782081e2f6b79cd1e3540d
John Gesimondo <john@jmondo.com>
2013-02-10 23:58:28 -0800

    correct answer woo

--- inversions.rb
+++ inversions.rb
@@ -3,13 +3,21 @@
 # 2. recursively order the right half (n/2 things)
 # 3. merge them back together
 # base case - if you have 0 or 1 elements in the array, return it
-c = [2,4,6,3,8,1,3,5]
+c = File.readlines('./IntegerArray.txt').collect(&:to_i)
+
+# a = [1,3,5,2,4,6]
+# b = [1,2,3,4,5,6]
+# c = [6,5,4,3,2,1]
+# d = [6,5,4,3,2,1,7]
+# e = [6,5,4,3,2,1,7,8]
+
 
 def merge_sort(c)
   # halve it
   split = c.length / 2 #half round down
   a = c[0..(split-1)] #left
   b = c[split..-1] #right
+  @inversions ||= 0
 
   a = merge_sort(a) unless a.length < 2
   b = merge_sort(b) unless b.length < 2
@@ -41,14 +49,15 @@ def merge_in_order(a,b)
     else
       c << b[j]
       j += 1
+      @inversions += (a.length - i)
     end
   end
-
   return c
 end
 
-p merge_sort(c)
-
+@inversions = 0
+merge_sort(c)
+puts @inversions
 
 # start with full size array, build 2 sub arrays each with half of original values
 # pass both to the merge thing
